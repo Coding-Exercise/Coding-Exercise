1acb53490578d2e28a897e361684d0bcf3f81a8f
trevorwinser <64448847+trevorwinser@users.noreply.github.com>
2024-04-07 13:36:03 -0700

    Created version with pagination

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 import json
+import re
     # -H "Accept: application/vnd.github+json" \
     # -H "Authorization: Bearer <YOUR-TOKEN>" \
     # -H "X-GitHub-Api-Version: 2022-11-28" \
@@ -11,23 +12,52 @@ def main():
     org = sys.argv[1]
     getRepositories(org)
 
-
-
 def getRepositories(org):
     token = ""
     with open("token.txt") as my_file:
         token = my_file.read()
     
-    headers = {'Authorization': 'token ' + token}
+    results = []
 
-    response = requests.get(f"https://api.github.com/orgs/{org}/repos", headers = headers)
+    headers = {'Authorization': 'token ' + token}
+    link = f"https://api.github.com/orgs/{org}/repos?per_page=1000"
+    while link:
+        response = requests.get(link, headers = headers)
+        for repo in response.json():
+                results.append({
+                    "repository": repo.get('name'),
+                    "secret_scanning": repo.get('security_and_analysis').get('secret_scanning').get('status') if repo.get('security_and_analysis') else "Unavailable"
+                })
+        link = None
+        if response.headers.get("Link"):
+            match = re.search('(?<=<)([\S]*)(?=>; rel="Next")', response.headers.get("Link"), re.IGNORECASE)
+            if match:
+                link = match.group()
+        
+            
+
     # print(json.dumps(response.json(),indent=2)) 
-    for repo in response.json():
-        print(f"Repository name: {repo.get('name')} Secret scanning: {repo.get('security_and_analysis').get('secret_scanning').get('status')}")
+    # print(response.json())
+    
+    
+        # print(f"Repository name: {repo.get('name')} Secret scanning: {repo.get('security_and_analysis').get('secret_scanning').get('status')}")
+    print(json.dumps(results, indent=4))
+ 
+    # Writing to sample.json
+    
+        
     
     # print(f"Repository name: {repo.get('name')} Secret scanning: {repo.get('security_and_analysis').get('secret_scanning').get('status')}")
     # AttributeError: 'NoneType' object has no attribute 'get' -> cannot access other people's organisations
     # SOLUTION: pagination with items per page
+
+'''
+    What would you do when using the tool to extract data for lots of organisations without hitting the rate limit?
+    
+    How would you good runtime performance when extracting data for lots of organisations
+    
+    How would you schedule such a tool to monitor a set of organisations on regular basis
+'''
 
 if __name__ == '__main__':
     main()
